d7ac9c2bda70bb8cc956024771ecc76c25a9b52a
Ethan Holland <ethanjholland@gmail.com>
2018-12-12 12:12:02 -0500

    converted to object oriented, pre major changes to storage structure

--- newtab/scripts/wikipedia.js
+++ newtab/scripts/wikipedia.js
@@ -1,21 +1,23 @@
-function main(){
-    chrome.storage.sync.get('wikipedia', (res)=>{
-        res=res.wikipedia //get to the data
-        console.log(res)
+function main(site){
+    key=site.key
+    console.log(key)
+    chrome.storage.sync.get(key, (res)=>{
+        res=res[key] //get to the data
         if(res && res.active){
             //make sure structure is setup properly
             if(!res.pages)res.pages={}
             if(!res.categories)res.categories={}
+            console.log(res)
 
             //title
-            var title=getTitle();
+            var title=site.getTitle();
             if(!res.pages[title]){
                 res.pages[title]=0
             }
             res.pages[title]+=1
     
             //categories
-            var cats=getCategories();
+            var cats=site.getCategories();
             for(let cat of cats){
                 if(!res.categories[cat]){
                     res.categories[cat]=0;
@@ -24,30 +26,18 @@ function main(){
             }
     
             //write out
-            chrome.storage.sync.set({wikipedia:res})
-        } 
-        console.log(res)
+            out={}; out[key]=res
+            chrome.storage.sync.set(out)
+        }
     })
 }
 
-function getTitle(){
-     return document.getElementById('firstHeading').innerText
-}
-
-function getCategories(){
-    var catElements=document.getElementById('mw-normal-catlinks').getElementsByTagName('li');
-    var cats=[];
-    for(var i=0; i<catElements.length; i++){
-        cats.push(catElements[i].innerText)
-    }
-    return cats;
-}
-
-function clearData(){
+function clearData(site){
     //clears all data - use with great caution
     console.log('data cleared')
+    var key=site.key
     chrome.storage.sync.set({
-        'wikipedia':{
+        key:{
             'active': true,
             'pages': {},
             'categories': {}
@@ -55,6 +45,23 @@ function clearData(){
     });
 }
 
+var wikipedia = {
+    key: "wikipedia",
+
+    getTitle: function(){
+        return document.getElementById('firstHeading').innerText
+    },
+   
+    getCategories: function(){
+        var catElements=document.getElementById('mw-normal-catlinks').getElementsByTagName('li');
+        var cats=[];
+        for(var i=0; i<catElements.length; i++){
+            cats.push(catElements[i].innerText)
+        }
+        return cats;
+    }
+};
+
 // chrome.storage.sync.remove('wikipedia')
 // clearData()
-main()
+main(wikipedia)
