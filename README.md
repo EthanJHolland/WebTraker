c99fb8257221e2f9bde60f9ca35ae8b92af53972
Ethan Holland <ethanjholland@gmail.com>
2018-12-19 11:15:21 -0500

    codebase restructuring, simplifying

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WebTraker
+#### possible additions
+* 538
+* medium
--- newtab/scripts/stackexchange.js
+++
@@ -1,7 +0,0 @@
-var titleParts = document.title.split("- ")
-const title = titleParts[titleParts.length-1]
-var tagElements = document.getElementsByClassName('post-tag');
-var tags=[]
-for(var i=0; i<tagElements.length; i++){
-    tags.push(tagElements[i].text)
-}
